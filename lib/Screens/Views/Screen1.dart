5e22b508a0f565db55c0c56daae03d8ec41e8bb3
Dan <njokudanny234@gmail.com>
2025-10-25 15:57:36 +0100

    Normalize line endings

---
+++ lib/Screens/Views/Screen1.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import '../On_Board/on_boarding.dart';
+
+class Screen1 extends StatefulWidget {
+  const Screen1({super.key});
+
+  @override
+  State<Screen1> createState() => _Screen1State();
+}
+
+class _Screen1State extends State<Screen1> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (BuildContext context) {
+        return const on_boarding();
+      }));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF339CFF),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.2,
+              width: MediaQuery.of(context).size.height * 01,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                image: AssetImage("images/logo.png"),
+              )),
+            )
+          ]),
+    );
+  }
+}
